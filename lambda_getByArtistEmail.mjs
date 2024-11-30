6b44f7363e96eb79b4691298cfb101e61a81a0c6
Andrea-Coa <andrea.coa@utec.edu.pe>
2024-11-30 17:47:39 -0500

    test token validation

--- lambda_getByArtistEmail.mjs
+++ lambda_getByArtistEmail.mjs
@@ -1,19 +1,68 @@
 import {DynamoDBClient} from '@aws-sdk/client-dynamodb';
+import { LambdaClient, InvokeCommand } from '@aws-sdk/client-lambda'
 import { QueryCommand, DynamoDBDocumentClient } from '@aws-sdk/lib-dynamodb';
+
 
 const client = new DynamoDBClient({});
 const docClient = DynamoDBDocumentClient.from(client);
+const lambdaClient = new LambdaClient({region: 'us-east-1'})
 
 // parece funcionar
 
 export const handler = async (event) => {
 
     // verificar token
-    const token = event['headers']['Authorization'];
-    console.log('token', token);
-
-    const artist_id = event["body"]["artist_id"];
+    const token = event['headers'].get('Authorization');
+    const artist_id = event["body"].get('artist_id');
     const tableName = process.env.TABLE_NAME;
+    stage = tableName.split('-')[0]
+
+    if (!token) {
+        return {
+            statusCode: 400,
+            message: 'Falta el encabezado Authorization'
+        }
+    }
+    console.log('token', token);
+    if (!artist_id) {
+        return {
+            statusCode: 400,
+            message: 'Falta el parámetro artist_id'
+        }
+    }
+
+    const payload = {
+        token: token,
+        artist_id: artist_id
+    }
+
+    lambda_funtion_name = `api-users-${stage}-ValidateToken`
+    const params = {
+        FunctionName: lambdaFunctionName,
+        InvocationType: 'RequestResponse',
+        Payload: JSON.stringify(payload)
+    };
+
+    try {
+        const command = new InvokeCommand(params);
+        const response = await lambdaClient.send(command);
+        const responsePayload = JSON.parse(new TextDecoder('utf-8').decode(response.Payload));
+        console.log('Lambda response:', responsePayload);
+    
+        if (!responsePayload.statusCode || responsePayload.statusCode !== 200) {
+            return {
+                statusCode: 403,
+                message: 'Forbidden'
+            };
+        }
+    } catch (error) {
+        console.error('Error invoking Lambda function:', error);
+        return {
+            statusCode: 403,
+            message: 'Forbidden'
+        };
+    }
+
 
     const queryCommand = new QueryCommand({
         TableName: tableName,
